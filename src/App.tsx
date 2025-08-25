ccd95b32a9c06c8911c4599c581bd17a059efebf
Jahanara Khatun <zarakhatun58@gmail.com>
2025-08-25 09:28:51 +0530

    update

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,26 @@ import CartPage from './pages/Cart'
 import CheckoutPage from './pages/Checkout'
 import Layout from './components/Layout'
 import ProductDetails from './pages/ProductDetails'
+import { loadStripe } from '@stripe/stripe-js'
+import { Elements } from '@stripe/react-stripe-js'
+
+const stripePromise = loadStripe(import.meta.env.VITE_STRIPE_PUBLISHABLE_KEY);
 
 export default function App(){
+  
   return (
     <Layout>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/cart" element={<CartPage />} />
-        <Route path="/checkout" element={<CheckoutPage />} />
+         <Route
+          path="/checkout"
+          element={
+            <Elements stripe={stripePromise}>
+              <CheckoutPage />
+            </Elements>
+          }
+        />
         <Route path="/product/:id" element={<ProductDetails />} />
       </Routes>
     </Layout>
