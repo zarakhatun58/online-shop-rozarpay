7596d0605606d4dd3ff539a810760c2eac2cab84
Jahanara Khatun <zarakhatun58@gmail.com>
2025-08-25 09:52:06 +0530

    update

--- src/App.tsx
+++ src/App.tsx
@@ -7,17 +7,23 @@ import ProductDetails from './pages/ProductDetails'
 import { loadStripe } from '@stripe/stripe-js'
 import { Elements } from '@stripe/react-stripe-js'
 
-// const stripePromise = loadStripe(import.meta.env.VITE_STRIPE_PUBLISHABLE_KEY);
-const stripePromise = loadStripe("pk_test_51RaaB2QpJYqNVrlfiZmHRPSkE1fLvrwQv9ZmRS2dxGB2Udsp6rxjPyWyYwVICMBWEZcqC2AaqmfvLtxx8GI8yd1T00WvxWAttL");
+const stripePromise = loadStripe(import.meta.env.VITE_STRIPE_PUBLISHABLE_KEY);
 
-export default function App(){
-  
+export default function App() {
+
   return (
     <Layout>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/cart" element={<CartPage />} />
-         <Route
+        <Route
+          path="/cart"
+          element={
+            <Elements stripe={stripePromise}>
+              <CartPage />
+            </Elements>
+          }
+        />
+        <Route
           path="/checkout"
           element={
             <Elements stripe={stripePromise}>
