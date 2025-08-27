0a9d70ee685528c388dc6fa4e66994ac7cc320c5
Jahanara Khatun <zarakhatun58@gmail.com>
2025-08-27 11:43:47 +0530

    update

--- vite.config.ts
+++ vite.config.ts
@@ -5,24 +5,21 @@ import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
 export default defineConfig({
   server: {
     allowedHosts: ['online-shop-rozarpay.onrender.com'],
-    host: "::",
-    port: 5173,
     proxy: {
       '/api': {
         target: 'https://online-shop-server-hy92.onrender.com',
         changeOrigin: true,
-        secure: false,
-      },
-    },
+        secure: false
+      }
+    }
   },
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+      '@': path.resolve(__dirname, './src')
+    }
+  }
 });
