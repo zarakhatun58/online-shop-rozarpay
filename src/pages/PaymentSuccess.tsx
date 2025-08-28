f9d981e4aef254e5f43960c9ef63dda8d4c9f7d7
Jahanara Khatun <zarakhatun58@gmail.com>
2025-08-28 16:59:07 +0530

    update

--- src/pages/PaymentSuccess.tsx
+++ src/pages/PaymentSuccess.tsx
@@ -135,6 +135,12 @@ useEffect(() => {
       >
         📦 View All Orders
       </button>
+      <button
+        onClick={() => navigate("/orders/:id")}
+        className="mt-6 px-5 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700"
+      >
+        📦 Track Orders
+      </button>
     </div>
   );
 }
