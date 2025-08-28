66be6ceec959ad4f8659317dadc3f26c8d5412e3
Jahanara Khatun <zarakhatun58@gmail.com>
2025-08-28 17:40:55 +0530

    update

--- src/pages/PaymentSuccess.tsx
+++ src/pages/PaymentSuccess.tsx
@@ -86,7 +86,7 @@ export default function PaymentSuccess() {
           📦 View all orders
         </button>
         <button
-          onClick={() => navigate("/orders/:id")}
+          onClick={() => navigate(`/orders/${currentOrderId}`)}
           className="mt-6 px-5 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700"
         >
           📦 Track Orders
